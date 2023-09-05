652774cf0874fba29e6176f9b4870b49c34c2379
Kemal1101 <lataxa.11@gmail.com>
2023-09-05 11:06:58 +0700

    terserah

--- MyFirstJavaGithub.java
+++ MyFirstJavaGithub.java
@@ -2,6 +2,7 @@ public class MyFirstJavaGithub {
     public static void main(String[] args) {
         System.out.println("Hello from github");
         System.out.println(56* 2);
+        System.out.println("hahaha");
 
     }
 }
