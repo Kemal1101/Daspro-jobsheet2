bf52d515a641efa945857c8ff3614ec2d1514597
Kemal1101 <144076077+Kemal1101@users.noreply.github.com>
2023-09-07 09:02:02 +0700

    Add files via upload

---
+++ pleasepoo.java
@@ -0,0 +1,21 @@
+public class pleasepoo {
+public static void main(String[] args) {
+    System.out.println("please lah ya");
+    System.out.println(33 * 53);
+    // semuanya 
+    /* semua mua mua*/
+    String name = "GUEGE";
+    int myNum = 15;
+    String onceName = "Mina";
+    String fullName = name + onceName;
+    System.out.println(myNum);
+    System.out.println(fullName);
+    int x = 20, y = 35, z = 39;
+    System.out.println(x * y * z);
+    int a, b, c;
+    a = b = c = 20;
+    System.out.println(a + b + c);
+
+
+}
+}
